f7eec69a1f3fe6779baff5688680abf68a7ce27e
Mykyta Maliarenko <m.maliarenko@smartweb.com.ua>
2022-05-01 14:39:12 +0300

    add simple ORM and models

---
+++ src/models.py
@@ -0,0 +1,124 @@
+import typing as t
+from dataclasses import dataclass
+from orm import AbstractModel, ModelMeta
+from psycopg2.extensions import cursor
+
+
+@dataclass
+class Region(AbstractModel):
+    name: str
+
+    META = ModelMeta(
+        table_name='region'
+    )
+
+
+@dataclass
+class Area(AbstractModel):
+    name: str
+
+    META = ModelMeta(
+        table_name='area'
+    )
+
+
+@dataclass
+class Territory(AbstractModel):
+    name: str
+    type: str
+
+    META = ModelMeta(
+        table_name='territory'
+    )
+
+
+@dataclass
+class Place(AbstractModel):
+    region_id: int
+    area_id: int
+    territory_id: int
+
+    META = ModelMeta(
+        table_name='place'
+    )
+
+    @classmethod
+    def get_or_create(
+        cls,
+        c: cursor,
+        region_name: str,
+        area_name: str,
+        territory_name: str
+    ) -> 'Place':
+        region = Region.get_from_db(c, name=region_name)
+        area = Area.get_from_db(c, name=area_name)
+        territory = Territory.get_from_db(c, name=territory_name)
+
+        assert region is not None, \
+            f'Region with name "{region_name}" doesnt exists'
+        assert area is not None, \
+            f'Area with name "{area_name}" doesnt exists'
+        assert territory is not None, \
+            f'Territory with name "{territory_name}" doesnt exists'
+
+        place_data = dict(
+            region_id=region.id,
+            area_id=area.id,
+            territory_id=territory.id
+        )
+
+        place = cls.get_from_db(c, **place_data)
+        if place is None:
+            place = Place(id=None, **place_data)
+            place.create(c)
+            return place
+        else:
+            return place
+
+
+@dataclass
+class EducationalInstitution(AbstractModel):
+    name: str
+    type: str
+    place_id: int
+    parent_body_name: t.Optional[str]
+
+    META = ModelMeta(
+        table_name='educational_institution'
+    )
+
+
+@dataclass
+class Subject(AbstractModel):
+    code: str
+    name: str
+
+
+@dataclass
+class Student(AbstractModel):
+    gender: str
+    birth_year: int
+    studying_lang: str
+
+    place_of_living_id: int
+    educational_institution_id: int
+
+    class_profile: str
+
+    META = ModelMeta(
+        table_name='student'
+    )
+
+
+@dataclass
+class Exam(AbstractModel):
+    subject_id: int
+    student_id: str
+    educational_institution_id: int
+
+    dpa_level: t.Optional[str]
+    adapt_scale: int
+
+    raw_score: float
+    score: float
+    school_score: int
---
+++ src/orm.py
@@ -0,0 +1,99 @@
+import typing as t
+from psycopg2.extensions import cursor
+from dataclasses import dataclass, fields
+
+
+@dataclass
+class ModelMeta:
+    table_name: str
+
+
+@dataclass
+class AbstractModel:
+    id: t.Optional[int]
+    META: t.ClassVar[ModelMeta]  # type: ModelMeta
+
+    def __init__(self, *args, **kwargs):
+        assert self.META is not None, \
+            f'{self.__class__.__name__} requires meta'
+
+        super(AbstractModel, self).__init__(*args, **kwargs)
+
+    @staticmethod
+    def _sql_repr_value(value: t.Any) -> str:
+        if isinstance(value, str):
+            return "'" + value.replace("'", "''") + "'"
+        elif value is None:
+            return 'null'
+        else:
+            return str(value)
+
+    @classmethod
+    def create_from_db_result(
+            cls,
+            _fields: list[str],
+            values: tuple[t.Any]
+    ) -> 'AbstractModel':
+        assert len(_fields) == len(values), \
+            'len of fields and values doesnt match'
+        return cls(**{
+            field_name: values[index]
+            for index, field_name in enumerate(_fields)
+        })
+
+    @classmethod
+    def get_from_db(
+            cls, c: cursor,
+            **kwargs
+    ) -> t.Optional['AbstractModel']:
+        field_names = [field.name for field in fields(cls)]
+        where_clause = ' and '.join([
+            f'{key} = {cls._sql_repr_value(value)}'
+            for key, value in kwargs.items()
+        ])
+        sql = f'select {",".join(field_names)} from {cls.META.table_name} ' \
+              f'where {where_clause};'
+
+        c.execute(sql)
+        db_res = c.fetchone()
+        return (
+            cls.create_from_db_result(field_names, db_res)
+            if db_res
+            else None
+        )
+
+    def create(self, c: cursor) -> 'AbstractModel':
+        field_names = [field.name for field in fields(self)]
+        if self.id is None:
+            field_names.pop(field_names.index('id'))
+
+        sql_model_values = ",".join([
+            self._sql_repr_value(getattr(self, field))
+            for field in field_names
+        ])
+
+        c.execute(
+            f'insert into {self.META.table_name} ({",".join(field_names)}) '
+            f'values ({sql_model_values}) returning id;'
+        )
+
+        _id, = c.fetchone()
+        self.id = _id
+        return self
+
+    @classmethod
+    def bulk_create(cls, c: cursor, models: list['AbstractModel']):
+        field_names = [field.name for field in fields(cls)]
+        sql_models_values: list[str] = []
+        for model in models:
+            sql_models_values.append(
+                '(' + ",".join([
+                    cls._sql_repr_value(getattr(model, field))
+                    for field in field_names
+                ]) + ')'
+            )
+
+        c.execute(
+            f'insert into {cls.META.table_name} ({",".join(field_names)}) '
+            f'values {",".join(sql_models_values)};'
+        )
