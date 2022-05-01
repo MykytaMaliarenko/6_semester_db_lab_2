a97f01581c0339a66f3cca34d9bd5d228012135e
Mykyta Maliarenko <m.maliarenko@smartweb.com.ua>
2022-05-01 14:38:24 +0300

    add create-educational-institution migration

---
+++ src/migrations/003.create-educational-institution.py
@@ -0,0 +1,23 @@
+from yoyo import step
+
+__depends__ = ['002.create-place']
+
+
+steps = [
+    step(
+        """
+        CREATE TABLE educational_institution (
+            id serial primary key, 
+            name varchar(500) not null,
+            type varchar(500) not null,
+            place_id integer not null, 
+            parent_body_name varchar(500),
+            CONSTRAINT fk_place_id
+              FOREIGN KEY(place_id) 
+              REFERENCES place(id)
+              ON DELETE RESTRICT
+        )
+        """,
+        "DROP TABLE educational_institution",
+    ),
+]
